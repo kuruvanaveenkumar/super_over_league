8f591d5182ed2ec045ecde06d1a5ce821f009b7c
Naveen <kuruvanaveen1@gmail.com>
2023-12-29 06:11:32 +0000

    super over league cd-1 I

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div>
+    <div className="bg-container">
+      <div>
+        <h1 className="main-heading">Super Over League</h1>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+          className="image-size"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+          className="image-size"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
